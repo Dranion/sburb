52085237c894cc8e47de2ec6ade6f97ecb84f4a7
adoringCatfish <horsenkat@gmail.com>
2015-10-27 17:44:31 -0700

    Create Save.java

---
+++ Save.java
@@ -0,0 +1,13 @@
+
+public class Save {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void load(){
+		System.out.println("unable to load at the current moment!");
+		System.out.println("sorry :P");
+	}
+
+}
