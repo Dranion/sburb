d5b272abe31f940b3cfc8c6360f4d5c86de215ef
adoringCatfish <horsenkat@gmail.com>
2015-10-28 11:53:46 -0700

    Update Act1.java

--- Act1.java
+++ Act1.java
@@ -3,6 +3,10 @@ public class Act1 {
 
 	public static void main(String[] args) {
 		System.out.println("");
+		System.out.println("WELCOME TO SBURB, "  + p1.fname.toUpperCase() + " " + pl.lname.toUpperCase() + "!");
+		System.out.println("Today is your " + pl.age() + "th birthday.");
+		System.out.println("You have recieved a game for your present, called SBURB.");
+		System.out.println("Thanks " + p2.fname() + "!"); 
 
 	}
 
