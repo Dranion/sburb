0329decca558c5e2168a6cce7c80e248d8485a7a
adoringCatfish <horsenkat@gmail.com>
2015-10-30 07:54:15 -0700

    Update GameMechanics.java

--- GameMechanics.java
+++ GameMechanics.java
@@ -17,4 +17,22 @@ public class GameMechanics {
 		}
 		return sum;
 	}
+	public static CheckInt(Scanner scanner, String prompt){
+		System.out.print(prompt);
+		while (!scanner.hasNextInt()) {
+			scanner.nextLine();   // throw away offending token
+		    	System.out.println("Please enter an integer.");
+		    	System.out.print(prompt);
+		}
+		return scanner.nextInt();
+	}
+	public static CheckString(Scanner scanner, String prompt){
+		System.out.print(prompt);
+		while(!scanner.hasNextString()){
+			scanner.nextLine();
+			System.out.println("Please enter a string.");
+			System.out.print(prompt);
+		}
+		return scanner.nextString();
+	}
 }
