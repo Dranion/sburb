1a4b88522cb6e3fe85b383474916662f91f25f23
adoringCatfish <horsenkat@gmail.com>
2015-11-06 15:39:40 -0800

    Update house.java
    Attempting to create a system where you generate the corners for the rooms randomly, and get the basic house layout from that. Will fill in with other objects later. Its currently in progress!
    
    Failed attempt are commented out below the code. ;-;

--- house.java
+++ house.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 class house {
-  List<String> line = new ArrayList<String>();
-  List<List<String>> scematics = new ArrayList<List<String>>();
+  String line = "";
+  List<String> scematics = new ArrayList<String>();
   int housey;
   int housex;
   public house() {
@@ -16,11 +16,33 @@ class house {
 	  housey = GameMechanics.CheckInt(scanner, "Please enter a number: ");
 	  System.out.println("How wide (x) would you like your house to be? ");
 	  housex = GameMechanics.CheckInt(scanner, "Please enter a number: ");
-	  for(int i = 1; i <= housey; i++){
-		  for(int j = 1; j <= housex; j++){
-			  
-		  }
+	  String tempstring = "";
+	  for(int i = 1; i <= housex; i++){
+		  tempstring += "=";
 	  }
+	  scematics.add(tempstring);
+	  for(int i = 1; i <= housey-2; i++){
+		  line = "=";
+		  for(int j = 1; j <= housex-2; j++){
+			  int x = random.nextInt(housex);
+			  int y = random.nextInt(housey);
+			  //did you mean: i am crying
+			  //im too lazy to design my own house..... death cometh....
+		}
+		line += "=";
+		scematics.add(line);
+		System.out.println("\n\n\n\n\nPRINTING IN PROGRESS HOUSE...");
+		System.out.println(scematics);
+	      for(int j = 0; j < scematics.size(); j++) {
+	          System.out.println(scematics.get(j));
+	      }
+
+	  }
+	  scematics.add(tempstring);
+	  System.out.println("\n\n\n\n -----------------\n  PRINTING ENTIRE HOUSE");
+      for(int i = 0; i < scematics.size(); i++) {
+          System.out.println(scematics.get(i));
+      }
 	 
   }
 }
@@ -41,3 +63,42 @@ class house {
 		  	ln = scanner.next();
 		  	scematics.add(ln);
 		  }while(!(ln.equals("/")));*/
+
+
+
+/*int temp = random.nextInt(50);
+			  if(temp == 1 || temp == 2 || temp == 4){
+				  line += "+";
+			  }
+			  else if(temp == 7){
+				  line += "%";
+			  }
+			  else if(temp == 8){
+				  line += "#";
+			  }
+			  else{
+				  if(temp <= 35) {
+					  //(line.charAt(line.length()-1) == '=' || scematics.get(i-1).charAt(line.length()-1) == '=') && 
+					  if(line.charAt(line.length() -1) == '=' || scematics.get(i-1).charAt(line.length()-1)== '='){
+						  boolean isConnect = false;
+						  for(int a = 1; line.length() - a >= 0; a++){
+							  isConnect = (line.charAt(line.length() - a) == '=' || scematics.get(i-1).charAt(line.length() -a) == '=') && !(line.charAt(line.length() - a) == '=' && scematics.get(i-1).charAt(line.length() -a) == '=');
+						  }
+						  if(isConnect){
+							  line += "=";
+						  }
+						  else {
+							  line += "-";
+						  }
+					  }
+					  else {
+						  line += "-"; 
+					  }
+
+				  }
+				  else {
+					  line += "-";
+				  }
+			  }
+			  System.out.println("PRINTING LINE...");
+			  System.out.println(line);*/
