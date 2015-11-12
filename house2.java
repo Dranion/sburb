68236ad4a991c2b90a43038d5eb42c3da3349d77
adoringCatfish <horsenkat@gmail.com>
2015-11-12 08:11:37 -0800

    Update house2.java
    (screaming continues)

--- house2.java
+++ house2.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
-//http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.97.4544&rep=rep1&type=pdf !!!!
-public class housee {
+
+public class house2 {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
+		Random rand = new Random();
 		List<String> housey = new ArrayList<String>();
 		
 		System.out.print("How long (y) would you like your house to be?");
@@ -21,10 +24,44 @@ public class housee {
 		for(int i = 1; i<= yhouse; i++){//GENERATES EMPTY HOUSE! 
 			housey.add(tempstr);
 		}
+		printHouse(housey);
 		
-		housey.get(housey.size()-1);
-		
+		//Generating Front Door! 
+		int tempran = rand.nextInt(housey.get(housey.size()-1).length()-1);
+	    char[] templine = housey.get(housey.size()-1).toCharArray();
+	    templine[tempran] = '+';
+	    
+	    //Generating first side wall! 
+	    housey.set(housey.size()-1, String.valueOf(templine));
+	    printHouse(housey);
+	    String templinee = addX(xhouse, tempran, templine, rand);
+	    housey.set(housey.size()-1, templinee);
+	    printHouse(housey);
+		int starty = housey.size() -1;
+		//int startx = 
 
+	}
+	public static void printHouse(List<String> housey){
+	    System.out.println("Printing house...");
+		for(int i = 0; i < housey.size(); i++){
+	    	System.out.println(housey.get(i));
+	    }
+	}
+	public static String addX(int xhouse, int tempran, char[] templine, Random rand){
+	    while(rand.nextInt(xhouse - tempran) + 1 > 1){
+	    	templine[tempran + 1] = '=';
+	    	System.out.println(tempran);
+	    	tempran++;
+	    }
+	    return String.valueOf(templine);
+	}
+	public static List<String> addY(int yhouse, int starty, int startx, List<String> housey, Random rand){
+	    for(int i = rand.nextInt(yhouse - starty) + starty; i < yhouse-1; i++){
+		    char[] templine = housey.get(i).toCharArray();
+		    templine[startx] = '=';
+		    housey.set(i, String.valueOf(templine));
+	    }
+	    return housey;
 	}
 
 }
