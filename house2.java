a8056999d850055041aa08571668e1d6e1c29b47
adoringCatfish <horsenkat@gmail.com>
2015-11-17 08:14:04 -0800

    Update house2.java
    doing beta! (go! go! go! go!)

--- house2.java
+++ house2.java
@@ -10,10 +10,10 @@ public class house2 {
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		Random rand = new Random();
-		List<String> housey = new ArrayList<String>();
 		
 		System.out.print("How long (y) would you like your house to be?");
 		int yhouse = scanner.nextInt();
+		String [] housey = new String[yhouse];
 		System.out.print("How wide (x) would you like your house to be?");
 		int xhouse = scanner.nextInt();
 		String tempstr = "";
@@ -22,78 +22,73 @@ public class house2 {
 			tempstr = tempstr + "-";
 		}
 		for(int i = 1; i<= yhouse; i++){//GENERATES EMPTY HOUSE! 
-			housey.add(tempstr);
+			housey[i-1] = tempstr;
 		}
 		printHouse(housey);
 		
 		//Generating Front Door! 
-		int tempran = rand.nextInt(housey.get(housey.size()-1).length()-1);
-	    char[] templine = housey.get(housey.size()-1).toCharArray();
+		int tempran = rand.nextInt(housey[housey.length-1].length()-1);
+	    char[] templine = housey[housey.length-1].toCharArray();
 	    templine[tempran] = '+';
 	    
 	    //Generating first side wall! 
-	    housey.set(housey.size()-1, String.valueOf(templine));
+	    housey[housey.length-1] =  String.valueOf(templine);
 	    printHouse(housey);
 	    String[] tempaddX =  addX(xhouse, tempran, templine, rand);
-	    housey.set(housey.size()-1, tempaddX[0]);
+	    housey[housey.length-1] =  tempaddX[0];
 	    printHouse(housey);
 	    
 		//Recording end of first wall vars 
-	    int starty = housey.size() -1;
+	    int starty = housey.length -1;
 		int startx =  Integer.parseInt(tempaddX[1]);
 		System.out.println("("+ startx + ", " + starty + ")");
 		//Generating first y wall!
-		List<List<String>> yret = addY(yhouse, starty, startx, housey, rand);
-		housey.addAll(yret.get(0));
-
+		starty = addY(yhouse, starty, startx, housey, rand);
+		tempaddX =  addX(xhouse, startx, housey[starty].toCharArray(), rand);
+	    housey[housey.length-1] =  tempaddX[0];
+		startx =  Integer.parseInt(tempaddX[1]);
 		printHouse(housey);
 
 	}
-	public static void printHouse(List<String> housey){
+	public static void printHouse(String[] housey){
 	    System.out.println("Printing house...");
-		for(int i = 0; i < housey.size(); i++){
-	    	System.out.println(housey.get(i));
+		for(int i = 0; i < housey.length; i++){
+	    	System.out.println(housey[i]);
 	    }
 	}
 	
 	
-	public static String[] addX(int xhouse, int tempran, char[] templine, Random rand){
-		System.out.println("STARTING TEMPRAN: " + tempran);
-	    while(rand.nextInt(xhouse - tempran) + 1 > 1){
-	    	System.out.println(tempran);
-	    	templine[tempran + 1] = '=';
-	    	System.out.println(tempran);
-	    	tempran++;
+	public static String[] addX(int xhouse, int startx, char[] templine, Random rand){
+		System.out.println("STARTING TEMPRAN: " + startx);
+	    while(rand.nextInt(xhouse - startx) + 1 > 1){
+	    	System.out.println(startx);
+	    	templine[startx + 1] = '=';
+	    	System.out.println(startx);
+	    	startx++;
 	    }
-	    System.out.println("ENDING TEMPRAN: " + tempran);
+	    System.out.println("ENDING TEMPRAN: " + startx);
         String ret[] = new String[2];
         ret[0]= String.valueOf(templine);
-        ret[1] =  "" + (tempran+1);
+        ret[1] =  "" + (startx+1);
         System.out.println(ret[1]);
         return ret; //returning two values at once
 	}
 	
 	
 	
-	public static List<List<String>> addY(int yhouse, int starty, int startx, List<String> housey, Random rand){
+	public static int addY(int yhouse, int starty, int startx, String[] housey, Random rand){
 		int i = 0;
 		System.out.println(starty - i);
 		int tempran = rand.nextInt(yhouse /2 + i );
 		while(tempran > 0 && starty - i > 0){
 			i++;
 			tempran--;
-			char[] templine = housey.get(starty - i).toCharArray();
+			char[] templine = housey[starty - i].toCharArray();
 			templine[startx-1] = '=';
-			housey.set(starty - i, String.valueOf(templine));
+			housey[starty - i] =  String.valueOf(templine);
 			System.out.println(tempran);
 		}
-		starty = starty - i;
-		List<List<String>> ret = {housey}; //CRYING @ JAVA AND VARIABLE TYPES 5EVER i use python not this nonsense.....
-		//{"continued", "screaming"} {"i", "dont", "know","how","lists","work","aaahhhhhh"} cjkkjkkk
-		List<String> rety = null;
-		rety.add(starty + "");
-		ret.add(rety);
-	    return ret;
+	    return starty - 1;
 	}
 
 }
